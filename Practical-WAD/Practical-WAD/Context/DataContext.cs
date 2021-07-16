428b6f3fd619ab6782d423d0127a81302e0d8f74
NguyenTrongSon1 <sonntth2003006@fpt.edu.vn>
2021-07-16 21:52:28 +0700

    Practical

---
+++ Practical-WAD/Practical-WAD/Context/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Practical_WAD.Models;
+
+namespace Practical_WAD.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("Practice")
+        {
+        }
+
+        
+        public DbSet<ClassRoom> ClassRooms { get; set; }
+
+        public DbSet<Exam> Exams { get; set; }
+
+        public DbSet<Faculty> Faculties { get; set; }
+
+        public DbSet<Subject> Subjects { get; set; }
+    }
+}
---
+++ Practical-WAD/Practical-WAD/Models/Exam.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Practical_WAD.Models
+{
+    public class Exam
+    {
+        public int SubjectID { get; set; } // khoa ngoai
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please Enter Start Time")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
+        public DateTime StartTime { get; set; }
+        [Required(ErrorMessage = "Please Enter Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "0:dd/MM/yyyy", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+        [Required(ErrorMessage = "Please Enter Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "0:mm", ApplyFormatInEditMode = true)]
+        public string Duration { get; set; }
+
+        
+        public int FacultyID { get; set; }
+        public int ClassRoomID { get; set; }
+
+        public virtual Subject Subject { get; set; }
+        public virtual Faculty Faculty { get; set; }
+        public virtual ClassRoom ClassRoom { get; set; }
+    }
+}
